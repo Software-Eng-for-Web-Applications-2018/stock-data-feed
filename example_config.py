6b135372bb3000f3bd5babbadd9fb07ba9b13ea5
Kevin <kpielacki@gmail.com>
2018-02-27 12:19:25 -0500

    Example configuraiton file

---
+++ example_config.py
@@ -0,0 +1,8 @@
+# Example config.py file containing sensitive information
+
+# AlphaVantage API Token
+ALPHA_VANTAGE_API_KEY = ''
+
+# SQL Alchemy SQL URI
+# http://flask-sqlalchemy.pocoo.org/2.3/config/
+MYSQL_DB_URI = '<flavor>://<user>:<password>@<host>:<port>/<db>'
